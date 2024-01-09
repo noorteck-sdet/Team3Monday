ba83da0b0013b07411b64f76aa66e16215df8877
User <User@DESKTOP-6JPNDEN.lan>
2024-01-09 12:42:55 -0500

    Added Pull Request

--- src/db/DbTest.java
+++ src/db/DbTest.java
@@ -22,6 +22,10 @@ public class DbTest {
 	public void add( int a, int b) {
 		System.out.println((a + b));
 	}
+	
+	public void String(String str) {
+		System.out.println("How we pull request");
+	}
 
 }
  
