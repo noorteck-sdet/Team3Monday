899a834bdc8937fe92eae9efed732feb8d880984
alishan <alishan@10.0.0.226>
2024-01-11 20:02:30 -0500

    method

--- src/db/DbTest.java
+++ src/db/DbTest.java
@@ -3,59 +3,67 @@ package db;
 public class DbTest {
 
 	public static void main(String[] args) {
-		
-		//Kaylas making cats 
+
+		// Kaylas making cats
 		cats();
 	}
-	
-	public void add() {
-		System.out.println( 1 + 2);
-	}
-	
-	public void subtract() {
-	System.out.println(1-2);
-	}
-	
-	public void divide() {
-		System.out.println(1/2);
-	}
-	//Soli
-	public void car() {
-		System.out.println( "BMW");  
-	}
-	//Soli Trying  
-   
-	 
-	
 
-	public void add (int a,int b,int c, int d ) {
-		System.out.println(a+b+c+d);
+	public void add() {
+		System.out.println(1 + 2);
+	}
+
+	public void subtract() {
+		System.out.println(1 - 2);
+	}
+
+	public void divide() {
+		System.out.println(1 / 2);
+	}
+
+	// Soli
+	public void car() {
+		System.out.println("BMW");
+	}
+	// Soli Trying
+
+	public void add(int a, int b, int c, int d) {
+		System.out.println(a + b + c + d);
 		// did not push
 	}
+
 	public void String(String str) {
 		System.out.println("How we pull request");
 	}
-	//Kayls cat mothod
-	
-	public static void cats () { 
+	// Kayls cat mothod
+
+	public static void cats() {
 		System.out.println(" Meow");
 	}
-	
+
 	// Return days of the week
 	public static String days(int num) {
-		if(num == 1) return "Monday";
-		else if(num == 2) return "Tuesday";
-		else if(num == 3) return "Wednesday";
-		else if(num == 4) return "Thursday";
-		else if(num == 5) return "Friday";
-		else if(num == 6) return "Saturday";
-		else if(num == 7) return "Sunday";
-		else return "Enter a number between 1-7";
+		if (num == 1)
+			return "Monday";
+		else if (num == 2)
+			return "Tuesday";
+		else if (num == 3)
+			return "Wednesday";
+		else if (num == 4)
+			return "Thursday";
+		else if (num == 5)
+			return "Friday";
+		else if (num == 6)
+			return "Saturday";
+		else if (num == 7)
+			return "Sunday";
+		else
+			return "Enter a number between 1-7";
 	}
 
 	public static void dogs() {
-		System.out.println("whisper bark");//Ellen
+		System.out.println("whisper bark");// Ellen
 	}
+
 	public void divide(int a, int b) {
 
 		System.out.println(a / b);
@@ -67,40 +75,41 @@ public class DbTest {
 		String result = "Git";
 		String s1 = "Down";
 
-		return result + s1;//-->Added by Brian
+		return result + s1;// -->Added by Brian
 
 	}
-	
+
 	public static boolean isKamyHere() {
 		boolean result = false;
-		
-		
+
 		return result; // --> added by alishan
 	}
-	
+
 	public static boolean isTeam3TheBest() {
 		boolean result = true;
-		
-		return result;//-->You already know this! Brian
-		
-		
-		
+
+		return result;// -->You already know this! Brian
+
 		// Hi Brian please review my codes --- Akbar
-		
-		
+
 	}
-	
+
 	public static void main1(String[] args) {
-		
+
 		System.out.println("We all love Kamy");
-		
+
 	}
-	
-	//this is going to kamy 
-	
-	//Jackie trying to get diamonds
+
+	// this is going to kamy
+
+	// Jackie trying to get diamonds
 
 	public static void getDiamonds(String diamonds) {
 		System.out.println("Where they at though?");
+
+	}
 	
+	public static void getDiamonds(String diamonds) {
+		System.out.println("Where they at though?");
+	}
 }
